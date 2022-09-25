56c1e08361d17ec9ef436d6f5801ec5f45f0794a
vtlk2012 <vtlk2012@gmail.com>
2022-09-25 21:13:46 +0300

    feat: MVP

---
+++ main.js
@@ -0,0 +1,101 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    hasShadow: false,
+    roundedCorners: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+
+  win.webContents.on('before-input-event', (event, input) => {
+    if (input.type === 'keyDown' && input.key === 'ArrowDown') {
+      const bounds = win.getBounds()
+
+      if (input.alt) {
+        win.setBounds({
+          height: bounds.height + (input.shift ? 10 : 1)
+        })
+
+        return
+      }
+
+      win.setBounds({
+        y: bounds.y + (input.shift ? 10 : 1)
+      })
+    }
+
+    if (input.type === 'keyDown' && input.key === 'ArrowUp') {
+      const bounds = win.getBounds()
+
+      if (input.alt) {
+        win.setBounds({
+          height: bounds.height - (input.shift ? 10 : 1)
+        })
+
+        return
+      }
+
+      win.setBounds({
+        y: bounds.y - (input.shift ? 10 : 1)
+      })
+    }
+
+    if (input.type === 'keyDown' && input.key === 'ArrowLeft') {
+      const bounds = win.getBounds()
+
+      if (input.alt) {
+        win.setBounds({
+          width: bounds.width - (input.shift ? 10 : 1)
+        })
+
+        return
+      }
+
+      win.setBounds({
+        x: bounds.x - (input.shift ? 10 : 1)
+      })
+    }
+
+    if (input.type === 'keyDown' && input.key === 'ArrowRight') {
+      const bounds = win.getBounds()
+
+      if (input.alt) {
+        win.setBounds({
+          width: bounds.width + (input.shift ? 10 : 1)
+        })
+
+        return
+      }
+
+      win.setBounds({
+        x: bounds.x + (input.shift ? 10 : 1)
+      })
+    }
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
